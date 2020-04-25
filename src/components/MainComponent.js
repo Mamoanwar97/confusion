75d7859c73c627585ebcc57192f538ac9bdcbee3
mamoanwar97 <mamoanwar97@gmail.com>
2020-04-25 17:42:10 +0200

    controlled forms and validation

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Menu from './MenuComponent';
 import Home from './HomeComponent';
 import Contact from './ContactComponent';
+import About from './AboutComponent';
 import DishDetail from './DishdetailComponent';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
@@ -35,6 +36,14 @@ class Main extends Component {
       );
     }
 
+    const AboutPage = () => {
+      return(
+          <About
+              leaders={this.state.leaders}
+          />
+      );
+    }
+
     const DishWithId = ({match}) => {
         return(
             <DishDetail dish={this.state.dishes.filter((dish) => dish.id === parseInt(match.params.dishId,10))[0]}
@@ -49,6 +58,7 @@ class Main extends Component {
             <Route path='/home' component={HomePage} />
             <Route exact path='/menu' component={() => <Menu dishes={this.state.dishes} />} />
             <Route path='/menu/:dishId' component={DishWithId} />
+            <Route exact path='/aboutus' component={AboutPage} />
             <Route exact path='/contactus' component={Contact} />} />
             <Redirect to="/home" />
           </Switch>
